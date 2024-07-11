31610f70f761e2be6f9f53bc239cec0a7c51e9bc
rahul <84310456+kurageGit@users.noreply.github.com>
2024-07-11 09:08:17 +0530

    spelling correct

--- src/app/components/Book/firstSection.tsx
+++ src/app/components/Book/firstSection.tsx
@@ -18,7 +18,7 @@ const FirstSection = React.memo(() => {
           <p className={classes.dec}>
           By Liza Pavlakos
           </p>
-          <h3 className={classes.sub_heading}>How She Overcame Her Traumatic Past And Found Her Prupose.</h3>
+          <h3 className={classes.sub_heading}>How She Overcame Her Traumatic Past And Found Her purpose</h3>
           <p className={classes.sub_dec}>
           Escaping sexual abuse at a young age, Liza faced homelessness. Domestic violence later left her physically scarred. Abducted and nearly murdered, she fell into depression and addiction. Yet, Liza rose above her traumas.  She became a successful entrepreneur and motivational speaker, using her story to inspire others to overcome adversity and achieve their full potential.
           </p>
